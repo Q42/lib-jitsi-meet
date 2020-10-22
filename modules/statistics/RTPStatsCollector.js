c7dc4ddd9c2ced34af7ab6be706531eb37d01bed
Ricardo Snoek <ricardo@q42.nl>
2020-10-22 13:50:54 +0200

    Making package work for correct version

--- modules/statistics/RTPStatsCollector.js
+++ modules/statistics/RTPStatsCollector.js
@@ -1,5 +1,5 @@
 import browser from '../browser';
-import { browsers } from 'js-utils';
+import { browsers } from '@jitsi/js-utils';
 
 import * as StatisticsEvents from '../../service/statistics/Events';
 import * as MediaType from '../../service/RTC/MediaType';
