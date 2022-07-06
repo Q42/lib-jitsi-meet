789cae3acdbf5ece1da164d272a7bf40146f5c8d
Benjamin <m0rph3v5@gmail.com>
2022-07-06 20:56:52 +0200

    Revert "Increase default screenshare resolution to 1080x1920"
    This reverts commit 01fa4442e2e37fcc4d1d8dccc16f695b2c2fba5f.

--- modules/RTC/ScreenObtainer.js
+++ modules/RTC/ScreenObtainer.js
@@ -222,7 +222,7 @@ const ScreenObtainer = {
         video.frameRate && delete video.frameRate.min;
 
         const constraints = {
-            video: { width: 1920, height: 1080 },
+            video: { width: 1280, height: 720 },
             audio,
             cursor: 'always'
         };
